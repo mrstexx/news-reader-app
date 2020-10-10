e99ed9a5f1226b3c8c9c3c103c0be959de09ad09
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-10-10 14:01:12 +0200

    add full story button

--- app/src/main/java/at/technikum_wien/miljevic/newsreader/details/DetailsActivity.java
+++ app/src/main/java/at/technikum_wien/miljevic/newsreader/details/DetailsActivity.java
@@ -2,8 +2,12 @@ package at.technikum_wien.miljevic.newsreader.details;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +28,9 @@ public class DetailsActivity extends AppCompatActivity {
     private TextView mDateText;
     private TextView mDescriptionText;
     private TextView mKeywordsText;
+    private Button mFullStoryBtn;
+
+    private NewsModel newsModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +43,17 @@ public class DetailsActivity extends AppCompatActivity {
         mDateText = findViewById(R.id.details_date);
         mDescriptionText = findViewById(R.id.details_description);
         mKeywordsText = findViewById(R.id.details_keywords);
+        mFullStoryBtn = findViewById(R.id.btn_full_story);
 
         if (getIntent().hasExtra(NewsHelper.NEWS_ITEM_EXTRA)) {
-            NewsModel newsModel = getIntent().getParcelableExtra(NewsHelper.NEWS_ITEM_EXTRA);
+            newsModel = getIntent().getParcelableExtra(NewsHelper.NEWS_ITEM_EXTRA);
             assert newsModel != null;
             mTitleText.setText(newsModel.getTitle());
             mAuthorText.setText(newsModel.getAuthor());
             mDateText.setText(NewsHelper.getLocalDate(newsModel.getPublicationDate()));
             mDescriptionText.setText(newsModel.getDescription());
             mKeywordsText.setText("#" + String.join(", ", newsModel.getKeywords()));
+            mFullStoryBtn.setOnClickListener(this::onFullStoryButtonClick);
             Glide.with(this)
                     .load(newsModel.getImage())
                     .onlyRetrieveFromCache(true)
@@ -52,8 +61,14 @@ public class DetailsActivity extends AppCompatActivity {
                     .error(R.drawable.ic_img_placeholder)
                     .into(mImageView);
         } else {
+            mFullStoryBtn.setClickable(false);
             Toast.makeText(this, R.string.no_available_data, Toast.LENGTH_SHORT).show();
             Log.w(TAG, "Intent has no news item data");
         }
+    }
+
+    private void onFullStoryButtonClick(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(newsModel.getLink()));
+        startActivity(browserIntent);
     }
 }
